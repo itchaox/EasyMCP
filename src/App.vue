2fbb7e4613ec282852a411796b909dc9b09069fe
Wang Chao <wangchao2423@gmail.com>
2025-04-24 20:01:42 +0800

    优化App.vue中的删除按钮样式，增加左侧边距以提升界面美观性

--- src/App.vue
+++ src/App.vue
@@ -51,31 +51,32 @@
                     </div>
                   </el-option>
                 </el-select>
-              </div>
 
-              <el-button
-                type="danger"
-                class="delete-btn"
-                :disabled="selectedServers.length === 0"
-                @click="deleteSelectedServers"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  width="16"
-                  height="16"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  stroke-width="2"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                <el-button
+                  style="margin-left: 20px"
+                  type="danger"
+                  class="delete-btn"
+                  :disabled="selectedServers.length === 0"
+                  @click="deleteSelectedServers"
                 >
-                  <path d="M3 6h18"></path>
-                  <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
-                  <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
-                </svg>
-                删除已选
-              </el-button>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="16"
+                    height="16"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    stroke-width="2"
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                  >
+                    <path d="M3 6h18"></path>
+                    <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6"></path>
+                    <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2"></path>
+                  </svg>
+                  删除已选
+                </el-button>
+              </div>
             </div>
           </div>
           <div class="editor-container">
